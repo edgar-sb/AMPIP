4b8343dc1f920e2ffa37353caed249c0b3b4f609
edgar martinez <edgar@space.bar>
2021-06-10 20:27:54 -0500

    ok

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -10,9 +10,9 @@ http://localhost:8000/api,
 */
 export default new Vuex.Store({
   state: {
-    url: "http://localhost:8000/api",
-    img: "http://localhost:8000/uploads",
-    baseUrl: "http://localhost:8000",
+    url: "http://18.207.162.106/api",
+    img: "http://18.207.162.106/uploads",
+    baseUrl: "http://18.207.162.106",
     data: null,
     permisos: null,
     token: "a270c78c-a633-4e22-bb98-172615781bb8",
