49642537dce951214deddd10c6b1e9dbc14ca869
edgar martinez <edgar@space.bar>
2021-06-11 10:37:06 -0500

    ok

--- public/mailler/index.php
+++ public/mailler/index.php
@@ -21,8 +21,8 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'moontsapp@gmail.com';                     //SMTP username
     $mail->Password   = 'GMEDVM2020';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('moontsapp@gmail.com', 'Ampip');
