08e61f7421ac767b7f353e020979880c440b4852
edgar martinez <edgar@space.bar>
2021-06-17 11:13:23 -0500

    build

--- createModulesPhp/in.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-miConexion = pymysql.connect( host='localhost', user= 'edy', passwd='123', db='ampip' )
-cur = miConexion.cursor()
-cur.execute( "SELECT * FROM user" )
-print(cur)
-miConexion.close()
