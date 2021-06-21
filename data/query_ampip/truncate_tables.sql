87bbd3a9c1be8ff731ea70ef87709a73550fdc11
edgar martinez <edgar@space.bar>
2021-06-21 14:06:35 -0500

    ok

--- data/query_ampip/truncate_tables.sql
+++ data/query_ampip/truncate_tables.sql
@@ -3,6 +3,11 @@ truncate table ampip.parque;
 truncate table ampip.inquilino_nave;
 truncate table ampip.parques_usuarios;
 truncate table ampip.space;
+truncate table ampip.espacio_dispobible;
+truncate table ampip.extras;
+truncate table ampip.locations;
+truncate table ampip.reclog;
+truncate table ampip.nave;
 delete from ampip.user where id != 1;
 delete from ampip.datosDeUsuario where id != 1;
 
